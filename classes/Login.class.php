d3b0655f96da7c461899967e9bd2df73f4b6e69e
Yacine <potatooryg3@gmail.com>
2022-04-06 23:10:59 +0100

    login

--- classes/Login.class.php
+++ classes/Login.class.php
@@ -19,5 +19,41 @@ class Login extends Dbh{
             exit();
         }
 
+        $hashedpwd =$stmt->fetchAll(PDO::FETCH_ASSOC);
+        $checkpwd = password_verify($password,$hashedpwd[0],["users_pwd"]);
+
+
+        if($checkpwd== false){
+            $stmt=null;
+            header('location: ../index.php?error=incorrectpassword');
+            exit();
+        }
+        elseif($checkpwd==true){
+            $stmt= $this->connect()->prepare('SELECT * FROM users WHERE users_username = ? OR users_email= ? AND users_pwd = ?;');
+
+            if(!$stmt->execute(array($username,$username,$password))){
+                $stmt = null;
+                header("localtion: ../index.php?error=stmtfailed");
+                exit();
+            }
+          
+            if($stmt->rowCount() == 0){
+
+                $stmt = null;
+                header('location: ../index.php?error=usernotfound');
+                exit();
+            }
+
+            $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            session_start();
+
+            $_SESSION["userid"] = $user[0]["users_id"];
+            $_SESSION["userun"] = $user[0]["users_username"];
+            $stmt = null;
+
+        }
+
+
     }
 }
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,16 +4,15 @@ if (isset($_POST['submit'])){
 
     $username= $_POST['username'];
     $pwd= $_POST['pwd'];
-    $pwdrepeat= $_POST['pwdrepeat'];
-    $email= $_POST['email'];
+    
 
     
 
     include 'class-autoload.inc.php';
 
-    $signup= new Logincontr($username,$pwd,$pwdrepeat,$email);
+    $loginup= new Logincontr($username,$pwd,$pwdrepeat,$email);
 
-    $signup->loginUser();
+    $loginup->loginUser();
     
     header("location: ../index.php?error=none");
 }
