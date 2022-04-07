2273d9d0c1595d4411c635995bcfb0f9861c25fd
Yacine <potatooryg3@gmail.com>
2022-04-07 05:51:11 +0100

    edit profile v1

--- classes/Edit_profile.class.php
+++ classes/Edit_profile.class.php
@@ -1,5 +1,53 @@
 <?php
 
 class Edit_profile extends Dbh{
-    
+
+  protected function changeUsername($username,$password){
+        $stmt= $this->connect()->prepare('SELECT users_pwd FROM users WHERE users_username = ?');
+        
+        
+        if(!$stmt->execute($username)){
+            $stmt = null;
+            header("localtion: ../index.php?error=stmtfailed");
+            exit();
+        }
+
+        if($stmt->rowCount() == 0){
+
+            $stmt = null;
+            header('location: ../index.php?error=usernotfound');
+            exit();
+        }
+
+        $hashedpwd =$stmt->fetchAll(PDO::FETCH_ASSOC);
+        $checkpwd = password_verify($password,$hashedpwd[0]["users_pwd"]);
+
+
+        if($checkpwd== false){
+            $stmt=null;
+            header('location: ../index.php?error=incorrectpassword');
+            exit();
+        }
+        elseif($checkpwd==true){
+            $stmt= $this->connect()->prepare('UPDATE users SET users_username = ? WHERE users.users_pwd=?');
+
+            if(!$stmt->execute(array($username,$password))){
+                $stmt = null;
+                header("localtion: ../profile.php?error=stmtfailed");
+                exit();
+            }
+          
+          
+
+            
+
+            session_start();
+
+            $_SESSION['Success']="Name Changed Successfully";
+            $stmt = null;
+
+        }
+
+
+    }
 }
--- includes/edit-profile.inc.php
+++ includes/edit-profile.inc.php
@@ -1,2 +1,16 @@
 <?php
 
+if(isset($_POST["submit"])){
+    $username = $_POST["new_name"];
+    $pwd = $_POST["pwd"];
+
+    include 'class-autoload.inc.php';
+
+
+      $editname= new Edit_profilecontr($username,$pwd);
+
+    $editname->editUser();
+    
+    header("location: ../profile.php?name_changed");
+
+}
